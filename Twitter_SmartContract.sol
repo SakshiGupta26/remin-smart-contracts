c5183800566c4adc803138a2583fbfe2248bcb4e
SakshiGupta26 <sakshigupta182226@gmail.com>
2026-06-04 23:30:16 +0530

    Add data structures for tweets and messages

--- Twitter_SmartContract.sol
+++ Twitter_SmartContract.sol
@@ -1,1 +1,20 @@
-
+  struct TweetDetails{
+    uint id;
+    address author;
+    string content;
+    uint creationTimestamp;
+    }
+    
+    struct Message{
+    uint id;
+    string content;
+    address sender;
+    address receiver;
+    uint timestamp;
+    }
+    
+    mapping(uint => TweetDetails) public tweets;
+    mapping(address =>uint[]) public tweetsof;
+    mapping(address=>mapping(address => Message[])) public conversation;
+    mapping(address => mapping(address => bool)) public operators;
+    mapping(address => address[]) public following;
