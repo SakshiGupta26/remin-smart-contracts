21700df27c376859ab037840eeae8eecb89b563a
SakshiGupta26 <sakshigupta182226@gmail.com>
2026-06-05 15:03:48 +0530

    Implement NFT Marketplace smart contract structure

--- NFTMarketplace_SmartContract.sol
+++ NFTMarketplace_SmartContract.sol
@@ -1,1 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFTMarketplace is ERC721URIStorage, ReentrancyGuard, Pausable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _marketIds;
+    Counters.Counter private _itemsSold;
+
+    address public owner;
+    uint256 public listingFee;
+    uint256 public platformFeeBps;
+
+    struct MarketItem {
+        uint256 marketId;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        address creator;
+        uint256 price;
+        uint96 royaltyBps;
+        bool listed;
+        bool sold;
+        uint256 timestamp;
+    }
+
+    struct Royalty {
+        address creator;
+        uint96 royaltyBps;
+    }
+
+    struct Auction {
+        uint256 tokenId;
+        address seller;
+        uint256 startPrice;
+        uint256 highestBid;
+        address highestBidder;
+        uint256 startTime;
+        uint256 endTime;
+        bool active;
+    }
+
+    mapping(uint256 => MarketItem) public marketItems;
+    mapping(uint256 => Royalty) public royalties;
+    mapping(uint256 => Auction) public auctions;
+
+    mapping(address => uint256) public pendingWithdrawals;
+
+    event MarketItemCreated(
+        uint256 marketId,
+        uint256 tokenId,
+        address seller,
+        uint256 price
+    );
+
+    event MarketItemSold(
+        uint256 tokenId,
+        address buyer,
+        uint256 price
+    );
+
+    event ItemListed(uint256 tokenId, uint256 price);
+    event ItemDelisted(uint256 tokenId);
+
+    event AuctionCreated(uint256 tokenId);
+    event BidPlaced(uint256 tokenId, uint256 bid);
+    event AuctionSettled(uint256 tokenId);
+
+    modifier onlyOwner() {
+        _;
+    }
+
+    constructor() ERC721("Elite NFT", "ENFT") {}
+
+    function mint(string memory uri, uint256 price, uint96 royaltyBps)
+        public
+        payable
+        returns (uint256)
+    {}
+
+    function listItem(uint256 tokenId, uint256 price) public {}
+
+    function buyItem(uint256 tokenId) public payable nonReentrant {}
+
+    function delistItem(uint256 tokenId) public {}
+
+    function resell(uint256 tokenId, uint256 price) public payable {}
+
+    function createAuction(
+        uint256 tokenId,
+        uint256 startPrice,
+        uint256 duration
+    ) public {}
+
+    function placeBid(uint256 tokenId) public payable {}
+
+    function settleAuction(uint256 tokenId) public {}
+
+    function setRoyalty(uint256 tokenId, uint96 royaltyBps) public {}
+
+    function withdraw() public {}
+
+    function pause() public {}
+
+    function unpause() public {}
+
+    function getMarketItems() public view returns (MarketItem[] memory) {}
+
+    function getMyNFTs() public view returns (MarketItem[] memory) {}
+
+    function getListedItems() public view returns (MarketItem[] memory) {}
+
+    receive() external payable {}
+}
